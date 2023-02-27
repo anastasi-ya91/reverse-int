1a6667f141b17b3f84d03a012aebe1236273bb0a
Anastasiya Markova <a.v.markova00@gmail.com>
2023-02-27 23:04:32 +0300

    add uncorrect solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  let reversed = 0;
+  let sign = Math.sign(n);
+  n = Math.abs(n);
   
+  while (n > 0) {
+    reversed = reversed * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+  
+  return sign * reversed; 
 }
